67fdaa5c660d6cbd23167f41c387119e085a7680
Ali Abid Zaidi <abidzaidi7@hotmail.com>
2022-05-12 17:26:29 +0400

    Adding type of packet conditiions

--- main.cpp
+++ main.cpp
@@ -34,8 +34,8 @@ std::string getProtocolTypeAsString(pcpp::ProtocolType protocolType)
 int main(int argc, char *argv[])
 {
 
-    // pcpp::IFileReaderDevice *reader = pcpp::IFileReaderDevice::getReader("QinQ.pcap.cap");
-    pcpp::IFileReaderDevice *reader = pcpp::IFileReaderDevice::getReader("dns_tcp.pcapng");
+    pcpp::IFileReaderDevice *reader = pcpp::IFileReaderDevice::getReader("QinQ.pcap.cap");
+    // pcpp::IFileReaderDevice *reader = pcpp::IFileReaderDevice::getReader("dns_tcp.pcapng");
 
     if (reader == NULL || !reader->open())
     {
@@ -46,71 +46,90 @@ int main(int argc, char *argv[])
     int packets = 0;
     pcpp::RawPacket rawPacket;
 
-    // if(!reader->getNextPacket(rawPacket)){
-    //     std::cerr << "Can't read first packet in the file" << std::endl;
-    //     return 2;
-    // }
 
     while (reader->getNextPacket(rawPacket))
     {
         packets++;
+        std::cout << "-------------------Packet " << packets << "-------------------" << std::endl;
         pcpp::Packet parsedPacket(&rawPacket);
 
         // Layer 2
         // Data Link Layer is not Ethernet drop!
-        pcpp::EthLayer* ethernetLayer = parsedPacket.getLayerOfType<pcpp::EthLayer>();
+        pcpp::EthLayer *ethernetLayer = parsedPacket.getLayerOfType<pcpp::EthLayer>();
         if (ethernetLayer == NULL)
         {
             std::cerr << "Something went wrong, couldn't find Ethernet layer" << std::endl;
-            return 1;
+            std::cout << "-----------------------------------------------\n"
+                      << std::endl;
+            continue;
         }
 
-        std::cout << std::endl
-                  << "Src  MAC address " << ethernetLayer->getSourceMac() << std::endl
-                  << "Dest MAC address " << ethernetLayer->getDestMac() << std::endl
-                  << "Ethernet type = 0x" << pcpp::netToHost16(ethernetLayer->getEthHeader()->etherType) << std::endl;
+        std::cout
+            << "Src  MAC address " << ethernetLayer->getSourceMac() << std::endl
+            << "Dest MAC address " << ethernetLayer->getDestMac() << std::endl
+            << "Ethernet type = 0x" << pcpp::netToHost16(ethernetLayer->getEthHeader()->etherType) << std::endl;
+
+        // bool hasVlan = parsedPacket.isPacketOfType<pcpp::VLAN>();
+        if (!parsedPacket.isPacketOfType(pcpp::VLAN))
+        {
+            std::cerr << "Doesn't contain a VLAN, pass!" << std::endl;
+            std::cout << "-----------------------------------------------\n"
+                      << std::endl;
+            continue;
+        }
 
         // Drop all packets except coming from vlanID
-        // pcpp::VlanLayer* vlanLayer = parsedPacket.getLayerOfType<pcpp::VlanLayer>();
-        // if (vlanLayer == NULL)
-        // {
-        //     std::cerr << "Vlan layer doesn't exist" << std::endl;
-        //     continue;
-        // }
 
-        // std::cout << "Vlan ID " << vlanLayer->getVlanID() << std::endl;
+        pcpp::VlanLayer *vlanLayer = parsedPacket.getLayerOfType<pcpp::VlanLayer>();
+        if (vlanLayer == NULL)
+        {
+            std::cerr << "Vlan layer doesn't exist" << std::endl;
+            std::cout << "-----------------------------------------------\n"
+                      << std::endl;
+            continue;
+        }
 
+        std::cout << "Vlan ID " << vlanLayer->getVlanID() << std::endl;
 
         // Layer 3
         // if packet ip version != argsIPVersion drop
 
-        pcpp::IPv4Layer* ipv4Layer = parsedPacket.getLayerOfType<pcpp::IPv4Layer>();
+        pcpp::IPv4Layer *ipv4Layer = parsedPacket.getLayerOfType<pcpp::IPv4Layer>();
 
-        if(ipv4Layer == NULL){
+        if (ipv4Layer == NULL)
+        {
             std::cerr << "Ipv4 Layer is missing" << std::endl;
+            std::cout << "-----------------------------------------------\n"
+                      << std::endl;
             continue;
         }
 
-        std::cout 
-            << "Src  IP: " << ipv4Layer->getSrcIPAddress() << std::endl 
-            << "Dest IP: " << ipv4Layer->getDstIPAddress() << std::endl 
+        std::cout
+            << "Src  IP: " << ipv4Layer->getSrcIPAddress() << std::endl
+            << "Dest IP: " << ipv4Layer->getDstIPAddress() << std::endl
             << "TTL: " << (int)ipv4Layer->getIPv4Header()->timeToLive << std::endl
-            << "IP Id: " << pcpp::netToHost16(ipv4Layer->getIPv4Header()->ipId) << std::endl; 
+            << "IP Id: " << pcpp::netToHost16(ipv4Layer->getIPv4Header()->ipId) << std::endl;
+
+        // TODO: ALter TTL to 60
 
         // If L4 = ICMP Drop
+        // bool hasICMP = parsedPacket.isPacketOfType<pcpp::ICMP>();
+        if (parsedPacket.isPacketOfType(pcpp::ICMP))
+        {
+            std::cerr << "Is an ICMP Packet, pass!" << std::endl;
+            std::cout << "-----------------------------------------------\n"
+                      << std::endl;
+            continue;
+        }
 
         // If L4 = UDP && DNS
-        // Then packet craft serverAddress & port replace by args fields
+        if (parsedPacket.isPacketOfType(pcpp::UDP) && parsedPacket.isPacketOfType(pcpp::DNS))
+        {
+            // Then packet craft serverAddress & port replace by args fields
+        }
 
-        // for (pcpp::Layer *curLayer = parsedPacket.getFirstLayer(); curLayer != NULL; curLayer = curLayer->getNextLayer())
-        // {
-        //     std::cout << "\t"
-        //         << "Layer type: " << getProtocolTypeAsString(curLayer->getProtocol()) << "; " // get layer type
-        //         << "Total data: " << curLayer->getDataLen() << " [bytes]; "                   // get total length of the layer
-        //         << "Layer data: " << curLayer->getHeaderLen() << " [bytes]; "                 // get the header length of the layer
-        //         << "Layer payload: " << curLayer->getLayerPayloadSize() << " [bytes]"         // get the payload length of the layer (equals total length minus header length)
-        //         << std::endl;
-        // }
+        std::cout << "-----------------------------------------------\n"
+                  << std::endl;
     }
 
     reader->close();
